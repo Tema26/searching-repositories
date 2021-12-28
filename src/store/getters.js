975bd84bbb52acd362300743dbcbe3a8b7c51ecc
Artem Moskvitin <moskvitin.artem@gmail.com>
2021-12-28 23:21:13 +0300

    add getters.js

---
+++ src/store/getters.js
@@ -0,0 +1,11 @@
+export default {
+   nameRepres(state) {
+      return state.searchNickname;
+   },
+   response(state) {
+      return state.arrayOfRepositoresDetails;
+   },   
+   returnPage(state) {
+      return state.page;
+   }
+}
